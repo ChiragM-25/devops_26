52bb87e63e5c044d56dba8b64541293a1dc28242
ChiragM-25 <chiragmadhu98@gmail.com>
2026-02-19 15:29:03 +0530

    svc script updated

--- jupiter_svc.sh
+++ jupiter_svc.sh
@@ -6,18 +6,18 @@ if [ -z "$service_name" ]; then
     exit 1
 fi
 
-sudo systemctl is-active --quiet "$service_name"
-if [ $? -ne 0 ]; then
-    echo "Service '$service_name' is not running. Attempting to restart..."
-    sudo systemctl restart "$service_name"
-    if [ $? -eq 0 ]; then
-        echo "Service '$service_name' restarted successfully."
-        exit 0
-    else
-        echo "Failed to restart service '$service_name'."
-        exit 1
-    fi
+if systemctl is-active --quiet "$service_name"; then
+	echo "Service '$service_name' is running."
 else
-    echo "Service '$service_name' is running."
-    exit 0
+	echo "Service '$service_name' is not running. Attempting to restart..."
+	echo " "
+	sudo systemctl restart "$service_name"
+	
+	if [[ $? -eq 0 ]]; then
+		echo "Service '$service_name' restarted successfully."
+		exit 0
+	else
+		echo "Failed to restart service $service_name."
+		exit 1
+	fi
 fi
